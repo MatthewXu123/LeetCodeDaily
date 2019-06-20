dc8109ff183eecd17578b342e629d9bedd2b1c38
Matthew Xu <matthew.xu@carel.locale>
2019-06-20 16:59:42 +0800

    0620u3

--- src/com/matthewxu/meduim/NoRepeatLongestStr.java
+++ src/com/matthewxu/meduim/NoRepeatLongestStr.java
@@ -6,7 +6,7 @@ package com.matthewxu.meduim;
  *
  */
 public class NoRepeatLongestStr {
-
+	//My idea
 	public int lengthOfLongestSubstring(String s) {
 		int targetLength = s.length();
 		// 如果传入的字符串为空字符串或者null,直接返回0.
@@ -16,27 +16,27 @@ public class NoRepeatLongestStr {
 		String tempStr = "";
 		for (int i = 0; i < targetLength; i++) {
 			char c1 = s.charAt(i);
-			for (int j = i + 1; j < targetLength; j++) {
-				char c2 = s.charAt(j);
-				String subStr = s.substring(i, j + 1);
-				int subStrLength = subStr.length();
-				// 如果从截取起点到目标字符串末尾的字串长度小于maxlength，则直接返回maxLength.
-				if (targetLength - i > maxLength) {
-					//如果截取的子串长度大于maxLength，进一步判断。
-					//if (subStr.length() > maxLength) {
-						//if (tempStr.length() != 0 && subStr.indexOf(tempStr) >= 0)
-							//break;
-						if (NoDuplicativeChar(subStr, c1) && NoDuplicativeChar(subStr, c2)) {
-							if (subStrLength > maxLength)
-								maxLength = subStrLength;
-						} else {
-							//tempStr = subStr;
-							break;
-						}
-					//}
-				} else
-					return maxLength;
-			}
+			// 如果从截取起点到目标字符串末尾的字串长度小于maxlength，则直接返回maxLength.
+			if (targetLength - i > maxLength) {
+				for (int j = i + 1; j < targetLength; j++) {
+					char c2 = s.charAt(j);
+					String subStr = s.substring(i, j + 1);
+					int subStrLength = subStr.length();
+					// 如果截取的子串长度大于maxLength，进一步判断。
+					// if (subStr.length() > maxLength) {
+					// if (tempStr.length() != 0 && subStr.indexOf(tempStr) >= 0)
+					// break;
+					if (NoDuplicativeChar(subStr, c1) && NoDuplicativeChar(subStr, c2)) {
+						if (subStrLength > maxLength)
+							maxLength = subStrLength;
+					} else {
+						// tempStr = subStr;
+						break;
+					}
+					// }
+				}
+			} else
+				return maxLength;
 		}
 		return maxLength;
 	}
@@ -47,6 +47,9 @@ public class NoRepeatLongestStr {
 		return false;
 	}
 
+	//其他解法
+	//方法一：暴力法
+	
 	public static void main(String[] args) {
 		String s = "nfpdmpi";
 		NoRepeatLongestStr noRepeatLongestStr = new NoRepeatLongestStr();
